2ea083b2aacc08a435196edbf8d7a0a01accb933
Tripp Weiner <TrippW@users.noreply.github.com>
2020-07-03 12:30:06 -0500

    PEP8 formatting

--- sts_relics.py
+++ sts_relics.py
@@ -17,12 +17,14 @@ RELIC_LINKS = [
     'https://slay-the-spire.fandom.com/wiki/Category:Beta_Relic'
 ]
 
+
 def log(text):
     """helper to log to file and print at the same time"""
     with open('sts_crawler.log', 'a') as logger:
         log_text = text.replace('\n', '\n\t')
         logger.write(f'\n{str(datetime.datetime.utcnow())}: {log_text}')
     print(text)
+
 
 class STSWikiReader:
     """Reads data from website, creates a lookup map of item names, and does
@@ -34,7 +36,7 @@ class STSWikiReader:
     cur = None
     max_name_word_cnt = 0
 
-    #For matching strings to items
+    # For matching strings to items
     max_match = 0
     strcmp = JaroWinkler()
 
@@ -54,7 +56,8 @@ class STSWikiReader:
 
     def _rm_symbol(self, name):
         """removes odd characters that should never be in a obj name"""
-        return name.replace('?', '').replace(',', '').replace('.', '').replace('!', '').replace('(', '').replace(')', '')
+        return name.replace('?', '').replace(',', '').replace('.', '') \
+            .replace('!', '').replace('(', '').replace(')', '')
 
     def _rm_squote(self, name):
         """removes single quotes"""
@@ -66,19 +69,23 @@ class STSWikiReader:
 
     def _rm_hyph(self, name):
         """swaps typical joining characters with spaces"""
-        return name.replace('-',' ').replace('_',' ')
+        return name.replace('-', ' ').replace('_', ' ')
 
     def _rm_beta(self, name):
         """removes beta tag (possible error from wiki)"""
-        return name.replace('_beta', '').replace('_Beta', '').replace('Beta', '').replace('beta', '')
+        return name.replace('_beta', '').replace('_Beta', '') \
+            .replace('Beta', '').replace('beta', '')
 
     def _gen_alternative_names(self, name):
-        """creates a massive list of possible mistypes for a specific name, used as an aid for matching user input"""
+        """creates a massive list of possible mistypes for a
+            specific name, used as an aid for matching user input
+        """
         names = set()
-        actions = [self._rm_symbol, self._rm_squote, self._lower, self._rm_hyph, self._rm_beta]
+        actions = [self._rm_symbol, self._rm_squote, self._lower,
+                   self._rm_hyph, self._rm_beta]
         for outer in range(len(actions)):
             temp_name = name
-            for inner in range(len(actions)-outer):
+            for inner in range(len(actions) - outer):
                 temp_name = actions[outer+inner](temp_name)
                 names.add(temp_name)
         return list(names)
@@ -88,34 +95,36 @@ class STSWikiReader:
         log(f'Updating {self.name}s...')
         seen_list = set()
 
-        #fetch data from links and update object with most recent info
+        # fetch data from links and update object with most recent info
         for link in self.links:
             res = requests.get(link, verify=False)
             for t in res.text.split('\n'):
                 if self.tag_match in t:
                     cur_name = self.parse(t)
                     seen_list.add(cur_name)
-                    #if we haven't seen it before, add it to our look up list.
-                    if ((not cur_name in self.base_set)
-                        and (not cur_name in self.ignore_list)
-                        and (not cur_name.startswith('Category:'))):
+                    # if we haven't seen it before, add it to our look up list.
+                    if (cur_name not in self.base_set)
+                    and (cur_name not in self.ignore_list)
+                    and (not cur_name.startswith('Category:')):
                         self.base_set.add(cur_name)
                         self.real_names.add(cur_name)
                         self.fake_name_map[cur_name] = cur_name
-                        self.max_name_word_cnt = max(self.max_name_word_cnt, len(cur_name.split(' ')))
-                        
+                        self.max_name_word_cnt = max(self.max_name_word_cnt,
+                                                     len(cur_name.split(' ')))
+
                         for new_name in self._gen_alternative_names(cur_name):
                             self.base_set.add(new_name)
                             self.fake_name_map[new_name] = cur_name
 
-        #handle deleted data from wiki
+        # handle deleted data from wiki
         recalc_max_name_word_cnt = False
         for cur_name in self.real_names - seen_list:
             for new_name in self._gen_alternative_names(cur_name):
                 self.base_set.remove(new_name)
                 del self.fake_name_map[new_name]
 
-            if not recalc_max_name_word_cnt and self.max_name_word_cnt == len(cur_name.split(' ')):
+            if not recalc_max_name_word_cnt
+            and self.max_name_word_cnt == len(cur_name.split(' ')):
                 recalc_max_name_word_cnt = True
             self.base_set.remove(cur_name)
             self.real_names.remove(cur_name)
@@ -124,38 +133,46 @@ class STSWikiReader:
         if recalc_max_name_word_cnt:
             self.max_name_word_cnt = 0
             for cur_name in self.real_names:
-                self.max_name_word_cnt = max(self.max_name_word_cnt, len(cur_name.split(' ')))
+                self.max_name_word_cnt = max(self.max_name_word_cnt,
+                                             len(cur_name.split(' ')))
 
-        #finalize update    
+        # finalize update
         self.last_update = datetime.datetime.utcnow()
         log('Found {} {}s'.format(len(self.real_names), self.name))
 
     def check_if_similar(self, name):
-        """uses similarity check to see if the passed in name may match any of our found or generated names"""
+        """uses similarity check to see if the passed in name may match
+            any of our found or generated names
+        """
         name = self.format_name(name)
         split_name = name.split(' ')
         word_thresh = 0.9**len(split_name)
         self.max_match = 0
         self.cur = None
         for item_name in self.base_set:
-            split_item_name  = item_name.split(' ')
+            split_item_name = item_name.split(' ')
             if len(split_name) == len(split_item_name):
                 word_check = 1
                 for i in range(len(split_name)):
-                    word_check *= self.strcmp.similarity(split_name[i], split_item_name[i])
-                    word_check *= self.strcmp.similarity(split_name[i][::-1], split_item_name[i][::-1])
+                    word_check *= self.strcmp.similarity(
+                        split_name[i], split_item_name[i])
+                    word_check *= self.strcmp.similarity(
+                        split_name[i][::-1], split_item_name[i][::-1])
 
                 if word_check > self.max_match:
                     self.max_match = word_check
                     if word_check >= word_thresh:
                         self.cur = self.fake_name_map[item_name]
-        return self.cur != None
+        return self.cur is not None
 
     def check_if_exists(self, name, update=True):
-        """Used to check if a name is a perfect match for any found names or is close enough to call a match"""
-        if update and datetime.datetime.utcnow() - self.last_update > datetime.timedelta(days=15):
+        """Used to check if a name is a perfect match for any found
+            names or is close enough to call a match
+        """
+        if update and datetime.datetime.utcnow() - self.last_update
+        > datetime.timedelta(days=15):
             self.update_info()
-            
+
         res = name in self.real_names
         if res:
             self.cur = name
@@ -168,28 +185,33 @@ class STSWikiReader:
             res = self.check_if_similar(name)
         return res
 
+
 class RedditBot:
-    last_update=None
+    last_update = None
+
     def __init__(self, readers):
         self.REDDIT = self.login()
         self.SUBREDDIT = self.REDDIT.subreddit('slaythespire')
         self.readers = readers
         self.NEW_LINE = '\n\n'
-        self.FIRST_REPLY_TEMPLATE = 'I am {:0.1f}% confident you mentioned {} in your post.'
+        self.FIRST_REPLY_TEMPLATE = 'I am {:0.1f}% confident you mentioned ' \
+                                    + '{} in your post.'
         self.REPLY_TEMPLATE = 'I am also {:0.1f}% confident you mentioned {}.'
-                              
+
         self.END_TEXT = 'Let me call the bot for you.' + \
                         self.NEW_LINE + '-'*50 + \
-                        self.NEW_LINE + "I am a bot response, but I am using my creator's account. " + \
-                            'Please reply to me if I got something wrong so he can fix it.' + \
-                        self.NEW_LINE + '[Source Code](https://github.com/TrippW/STS-Crawler)'
+                        self.NEW_LINE + "I am a bot response, but " + \
+                        "I am using my creator's account. " + \
+                        'Please reply to me if I got something wrong ' + \
+                        'so he can fix it.' + self.NEW_LINE + \
+                        '[Source Code](https://github.com/TrippW/STS-Crawler)'
 
     def login(self):
         """
         log in to reddit
         uses a praw.ini file to hold sensitive information
         """
-        return praw.Reddit(redirect_uri='http://localhost:8080', \
+        return praw.Reddit(redirect_uri='http://localhost:8080',
                            user_agent='STS Scraper by /u/devTripp')
 
     def start(self):
@@ -197,8 +219,9 @@ class RedditBot:
         while True:
             log('Starting up...')
             try:
-                if False and datetime.datetime.utcnow() - self.last_update  > datetime.timedelta(days=1):
-                    pass#self.update_ignore_files()
+                if False and datetime.datetime.utcnow() - self.last_update
+                > datetime.timedelta(days=1):
+                    self.update_ignore_files()
                 for post in self.SUBREDDIT.stream.submissions():
                     self.process_submission(post)
 
@@ -206,7 +229,9 @@ class RedditBot:
                 log(e)
 
     def update_ignore_files(self):
-        """finds ignore files for our reader and pulls data into the readers. Used to update ignored strings during runtime"""
+        """finds ignore files for our reader and pulls data into the readers.
+            Used to update ignored strings during runtime
+        """
         for reader in self.readers:
             fname = f'{reader.name}.ignore'
             if os.path.exists(fname):
@@ -218,10 +243,13 @@ class RedditBot:
         self.last_update = datetime.datetime.utcnow()
 
     def check_all_word_combos(self, title, on_true):
-        """breaks the sentence/title into words/groups of words, and tries to match it with data in a reader"""
+        """breaks the sentence/title into words/groups of words, and tries
+            to match it with data in a reader
+        """
 
         for reader in self.readers:
-            if datetime.datetime.utcnow() - reader.last_update > datetime.timedelta(days=15):
+            if datetime.datetime.utcnow() - reader.last_update
+            > datetime.timedelta(days=15):
                 reader.update_info()
             words = title.split(' ')
             mentions = dict()
@@ -235,23 +263,23 @@ class RedditBot:
                         if not found:
                             log(title)
                         cur = reader.cur
-                        print('Relic Mention: {} | {:0.2f}'.format(cur, rreader.max_match))
+                        print('Relic Mention: {} | {:0.2f}'.format(
+                            cur, rreader.max_match))
                         if cur in mentions.keys():
-                            mentions[cur] = max(reader.max_match*100, mentions[cur])
+                            mentions[cur] = max(reader.max_match*100,
+                                                mentions[cur])
                         else:
                             mentions[cur] = reader.max_match*100
                         found = True
-
         if found:
             on_true(mentions)
-
         return found
 
     def post_reply(self, items):
-        """formats and posts the data to reddit"""                
+        """formats and posts the data to reddit"""
         reply = ""
         grouped_items = dict()
-        #group by percent
+        # group by percent
         for key in items:
             k = int(items[key]*10)
             if k not in grouped_items.keys():
@@ -262,25 +290,27 @@ class RedditBot:
         for key in sorted(grouped_items, reverse=True):
             values = grouped_items[key]
             if len(values) == 1:
-                reply += template.format(key/10, f'[[{values[0]}]]') + self.NEW_LINE
+                reply += template.format(key/10, f'[[{values[0]}]]') + \
+                         self.NEW_LINE
             else:
                 item_list = ', '.join([f'[[{k}]]' for k in values[:-1]])
-                #oxford comma
+                # oxford comma
                 if len(values) != 2:
-                       item_list += ','
+                    item_list += ','
                 item_list += ' and ' + f'[[{values[-1]}]]'
                 reply += template.format(key/10, item_list) + self.NEW_LINE
             template = self.REPLY_TEMPLATE
 
         log(reply)
 
-        ###DEBUG TEXT###
+        # ## DEBUG TEXT# ##
         global time_at_run
         log(f'last time sts_relics was run: {time_at_run}')
         log(f'last time ignores were updated: {self.last_update}')
         for reader in self.readers:
-            log(f'last time {reader.name} reader was updated: {reader.last_update}')
-        ###END DEBUG###
+            log(f'last time {reader.name} reader was updated:' +
+                ' {reader.last_update}')
+        # ## END DEBUG# ##
         reply += self.END_TEXT
         self.post.reply(reply)
 
@@ -296,15 +326,16 @@ class RedditBot:
             self.check_all_word_combos(title, self.post_reply)
             checked_ids.append(post.id)
             with open('checked.txt', 'a') as f:
-                f.write('\n'+post.id)
-            
+                f.write('\n' + post.id)
+
+
 checked_ids = []
 can_post = True
 time_at_run = datetime.datetime.utcnow()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     def relic_parse(string):
-        title_start = string.index('title=')+7
+        title_start = string.index('title=') + 7
         title_end = string.index('"', title_start)
         if string.count('(', title_start) > 0:
             title_end = min(string.index('(', title_start)-1, title_end)
@@ -315,20 +346,25 @@ if __name__=='__main__':
             with open(filename, 'r') as f:
                 return [k.strip() for k in f.readlines()]
 
-    #Read from files            
+    # Read from files
     checked_ids = get_data('checked.txt')
     RELIC_IGNORE = get_data('relic.ignore')
 
-    #Setup and run bot
+    # Setup and run bot
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-    RelicReader = STSWikiReader('relic', RELIC_LINKS, 'category-page__member-link', RELIC_IGNORE, relic_parse)
-    redditbot = RedditBot([ RelicReader ])
+    RelicReader = STSWikiReader('relic',
+                                RELIC_LINKS,
+                                'category-page__member-link',
+                                RELIC_IGNORE,
+                                relic_parse)
+    redditbot = RedditBot([RelicReader])
     redditbot.start()
 
-    ###FOR TESTING#######################
+    # ##FOR TESTING#######################
     #
-    #class tempPost:
+    # class tempPost:
     #    def __init__(self, title, _id):
     #        self.title = title
     #        self.id = _id
-    #redditbot.process_submission(tempPost('Mummified Hand, Amplify, Astrolabe: Creative AI)', '1'))
+    # redditbot.process_submission(tempPost(
+    #    'Mummified Hand, Amplify, Astrolabe: Creative AI)', '1'))
